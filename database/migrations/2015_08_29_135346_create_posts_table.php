969415725ae23db62f6e505bf91bf8fc12d4962e
Romain <romain@Romains-iMac.local>
2015-08-29 16:16:23 +0200

    initial commit

---
+++ database/migrations/2015_08_29_135346_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('title');
+            $table->text('content');
+            $table->integer('author_id');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+            $table->integer('count_read');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
